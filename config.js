a1ea3c29e83673d99fbbe762c406f1086b1695d7
Tick-Tech <Tick-Tech@users.noreply.github.com>
2016-09-08 19:52:16 +0930

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+//this file contains basics like channel, nickname for the bot etc.
+var config = {
+        channels: ["#Channel1", "#channel2"],  //Add as many channels as you want.
+        server: "irc.putyourserverhere.com", //Add your server here.
+        botName: "Myname", //Choose a nick for your bot.
+};
+
+module.exports = config; //allows us to include the file into another file.
+
+
